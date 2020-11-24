f3b9f4ef7edeb42aceb81b83ae8b0b85e10cd35b
Dmytro Petrenko <dmg.petrenko@gmail.com>
2020-11-24 08:42:16 +0200

    basic-setup - stryker.conf upd

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,17 +1,14 @@
-module.exports = function (config) {
-  config.set({
-    packageManager: 'npm',
-    reporters: ['html', 'clear-text', 'progress'],
-    tsconfigFile: 'tsconfig.json',
-    testRunner: 'mocha',
-    mochaOptions: {
-      spec: ['test/**/*.spec.ts'],
-      require: ['ts-node/register', 'source-map-support/register'],
-      ui: 'bdd',
-      timeout: 35000,
-    },
-    coverageAnalysis: 'perTest',
-    mutate: ['src/**/*.ts'],
-    symlinkNodeModules: false,
-  });
+module.exports = {
+  packageManager: 'npm',
+  reporters: ['html', 'clear-text', 'progress'],
+  tsconfigFile: 'tsconfig.json',
+  testRunner: 'mocha',
+  mochaOptions: {
+    spec: ['test/**/*.spec.ts'],
+    require: ['ts-node/register', 'source-map-support/register'],
+    ui: 'bdd',
+  },
+  coverageAnalysis: 'perTest',
+  mutate: ['src/**/*.ts'],
+  symlinkNodeModules: false,
 };
