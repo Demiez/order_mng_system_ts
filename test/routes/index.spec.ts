021a2f48599dfcbafd69c7bbb68f4bd37e8b00da
Dmytro Petrenko <dmg.petrenko@gmail.com>
2020-11-25 09:57:37 +0200

    OMS-01 - unit tests for user.spec.ts

--- test/routes/index.spec.ts
+++ test/routes/index.spec.ts
@@ -1,22 +1,40 @@
 import * as chai from 'chai';
 import chaiHttp = require('chai-http');
+import 'chai/register-should';
 import 'mocha';
 import app from '../../src/app';
 
 chai.use(chaiHttp);
 
 const expect = chai.expect;
-chai.should();
 
 describe('baseRoute', () => {
   it('should respond with HTTP 200 status', async () => {
-    const res = await chai.request(app).get('/index');
+    const res = await chai
+      .request(app)
+      .get('/index')
+      .catch((err) => {
+        if (err.response) {
+          return err.response as Response;
+        } else {
+          throw err;
+        }
+      });
 
     expect(res.status).to.be.equal(200);
   });
 
   it('should respond with success message', async () => {
-    const res = await chai.request(app).get('/index');
+    const res = await chai
+      .request(app)
+      .get('/index')
+      .catch((err) => {
+        if (err.response) {
+          return err.response as Response;
+        } else {
+          throw err;
+        }
+      });
 
     res.body.should.be.an('object');
     expect(res.body.status).to.be.equal('success');
--- test/routes/user.spec.ts
+++ test/routes/user.spec.ts
@@ -1,11 +1,11 @@
 import * as chai from 'chai';
-import chaiHttp = require('chai-http');
+import 'chai/register-should';
 import 'mocha';
 import { v4 } from 'uuid';
 import app from '../../src/app';
 import { User } from '../../src/models';
 
-const user: User = {
+const user01: User = {
   id: v4(),
   username: 'Bob',
   firstName: 'Bob',
@@ -15,3 +15,65 @@ const user: User = {
   phone: '1111-222-333',
   userStatus: 1,
 };
+
+const validateUser = (user: User) => {
+  user.should.have.property('id');
+  user.id.should.be.a('String');
+
+  user.should.have.property('username');
+  user.username.should.be.a('String');
+
+  user.should.have.property('firstName');
+  user.firstName.should.be.a('String');
+
+  user.should.have.property('lastName');
+  user.lastName.should.be.a('String');
+
+  user.should.have.property('email');
+  user.email.should.be.a('String');
+
+  user.should.have.property('password');
+  user.password.should.be.a('String');
+
+  user.should.have.property('phone');
+  user.phone.should.be.a('String');
+
+  user.should.have.property('userStatus');
+  user.phone.should.be.a('Number');
+};
+
+describe('userRoute', () => {
+  it('should respond with HTTP 404 status because there is no user', async () => {
+    const res = await chai
+      .request(app)
+      .get(`/users/${user01.username}`)
+      .catch((err) => {
+        if (err.response) {
+          return err.response as Response;
+        } else {
+          throw err;
+        }
+      });
+
+    res.should.be.equal(404);
+  });
+
+  it('should create a new user and retrieve it back', async () => {
+    const res = await chai
+      .request(app)
+      .post('/users/')
+      .send(user01)
+      .catch((err) => {
+        if (err.response) {
+          return err.response as Response;
+        } else {
+          throw err;
+        }
+      });
+
+    res.status.should.be.equal(201);
+    res.body.should.be.an('object');
+    validateUser(res.body);
+    res.body.username.should.be.equal(user01.username);
+  });
+});
